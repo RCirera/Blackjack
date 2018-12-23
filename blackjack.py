ab445e9fa3b65298917de785a26a32ad3d075de8
RCirera <ricardcirera@gmail.com>
2018-12-23 19:20:33 +0100

    Added counters for dealer and player wins. These reset every time the script is run. Also changed the positions widgets

--- blackjack.py
+++ blackjack.py
@@ -20,37 +20,47 @@ dealer_hand_label = ttk.Label(root, text="Dealer's Hand:", width=12)
 dealer_hand_label.grid(row=0, column=0)
 dealer_count_label = ttk.Label(root, text="Dealer's Count:", width=12)
 dealer_count_label.grid(row=1, column=0)
+dealer_wins_label = ttk.Label(root, text="Dealer Wins:", width=12)
+dealer_wins_label.grid(row=4, column=0)
 
 player_hand_label = ttk.Label(root, text="Player's Hand:", width=12)
-player_hand_label.grid(row=2, column=0)
+player_hand_label.grid(row=0, column=2)
 player_count_label = ttk.Label(root, text="Player's Count:", width=12)
-player_count_label.grid(row=3, column=0)
+player_count_label.grid(row=1, column=2)
+player_wins_label = ttk.Label(root, text="Player Wins:", width=12)
+player_wins_label.grid(row=4, column=2)
 
 # Create the variables
 dealer_hand_str = tk.StringVar()
 dealer_count_int = tk.IntVar()
 dealer_count_int.set(dealer_hand.value)
+dealer_wins_int = tk.IntVar()
 
 player_hand_str = tk.StringVar()
 player_count_int = tk.IntVar()
 player_count_int.set(player_hand.value)
+player_wins_int = tk.IntVar()
 
 game_state_str = tk.StringVar()
 game_state_str.set("Click \'New Game\' to start")
 
 # Create the variable labels
-dealer_hand_var_label = ttk.Label(root, textvariable=dealer_hand_str)
+dealer_hand_var_label = ttk.Label(root, textvariable=dealer_hand_str, width=12, anchor="center")
 dealer_hand_var_label.grid(row=0, column=1)
 dealer_count_var_label = ttk.Label(root, textvariable=dealer_count_int)
 dealer_count_var_label.grid(row=1, column=1)
+dealer_wins_var_label = ttk.Label(root, textvariable=dealer_wins_int)
+dealer_wins_var_label.grid(row=4, column=1)
 
-player_hand_var_label = ttk.Label(root, textvariable=player_hand_str, anchor="w")
-player_hand_var_label.grid(row=2, column=1)
+player_hand_var_label = ttk.Label(root, textvariable=player_hand_str, width=12, anchor="center")
+player_hand_var_label.grid(row=0, column=3)
 player_count_var_label = ttk.Label(root, textvariable=player_count_int)
-player_count_var_label.grid(row=3, column=1)
+player_count_var_label.grid(row=1, column=3)
+player_wins_var_label = ttk.Label(root, textvariable=player_wins_int)
+player_wins_var_label.grid(row=4, column=3)
 
 game_state_var_label = ttk.Label(root, textvariable=game_state_str)
-game_state_var_label.grid(row=4, column=0, columnspan=2)
+game_state_var_label.grid(row=2, column=0, columnspan=4)
 
 # Define the button functions
 
@@ -92,18 +102,19 @@ def new_game():
 
     hit_button.config(state=tk.NORMAL)
     stand_button.config(state=tk.NORMAL)
+    new_game_button.config(state=tk.DISABLED)
     return
 
 
 # Create the buttons
 hit_button = ttk.Button(root, text="Hit", command=hit, state=tk.DISABLED)
-hit_button.grid(row=5, column=0)
+hit_button.grid(row=3, column=0)
 stand_button = ttk.Button(root, text="Stand", command=stand, state=tk.DISABLED)
-stand_button.grid(row=5, column=1)
-new_gameButton = ttk.Button(root, text="New Game", command=new_game)
-new_gameButton.grid(row=6, column=0)
+stand_button.grid(row=3, column=1)
+new_game_button = ttk.Button(root, text="New Game", command=new_game)
+new_game_button.grid(row=3, column=2)
 exit_button = ttk.Button(root, text="Exit", command=quit)
-exit_button.grid(row=6, column=1)
+exit_button.grid(row=3, column=3)
 
 # Add some space between the widgets
 for child in root.winfo_children():
@@ -115,20 +126,29 @@ for child in root.winfo_children():
 def check_player_bust():
     if player_hand.value > 21:
         game_state_str.set("Player Busted! Dealer Wins")
-        
+        dealer_wins_int.set(dealer_wins_int.get() + 1)
+
         hit_button.config(state=tk.DISABLED)
         stand_button.config(state=tk.DISABLED)
+        new_game_button.config(state=tk.NORMAL)
 
 
 def check_game_state():
     if dealer_hand.value > 21:
         game_state_str.set("Dealer Busted! Player Wins!")
+        player_wins_int.set(player_wins_int.get() + 1)
+        new_game_button.config(state=tk.NORMAL)
     elif player_hand.value > dealer_hand.value:
         game_state_str.set("Player Wins!")
+        player_wins_int.set(player_wins_int.get() + 1)
+        new_game_button.config(state=tk.NORMAL)
     elif player_hand.value == dealer_hand.value:
         game_state_str.set("Tie!")
+        new_game_button.config(state=tk.NORMAL)
     else:
         game_state_str.set("Dealer Wins!")
+        dealer_wins_int.set(dealer_wins_int.get() + 1)
+        new_game_button.config(state=tk.NORMAL)
 
 
 # Activate GUI loop
